9a1b60bcc0e4879e0ac85ec8c17cc0b6ff0a3ae3
Kyrylo Kozlovskyi <G00425385@atu.ie>
2025-11-12 12:42:26 +0000

    Format code

--- src/ie/atu/sw/NodeView.java
+++ src/ie/atu/sw/NodeView.java
@@ -8,10 +8,10 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 
-public class NodeView extends Canvas{
-	private Node<Integer>[][] model = null; //This is really the same as Object[][]
+public class NodeView extends Canvas {
+	private Node<Integer>[][] model = null; // This is really the same as Object[][]
 	private Visitor<Integer> visitor = new Visitor<>();
-	
+
 	public NodeView(Node<Integer>[][] model, int width, int height) {
 		super(width, height);
 		this.model = model;
@@ -24,29 +24,30 @@ public class NodeView extends Canvas{
 
 	public void paint() {
 		GraphicsContext g = super.getGraphicsContext2D();
-		
+
 		PixelWriter pw = g.getPixelWriter();
 		for (int row = 0; row < model.length; row++) {
 			for (int col = 0; col < model[row].length; col++) {
 				model[row][col].accept(visitor);
 				pw.setColor(col, row, valueOf(model[row][col].getValue()));
 			}
-		}	
+		}
 	}
-	
+
 	private Color valueOf(int pixel) {
-		//ARGB Mode
+		// ARGB Mode
 		int mask = 0x000000FF;
-		//int alpha = (pixel >>> 24);
+		// int alpha = (pixel >>> 24);
 		int red = (pixel >>> 16) & mask;
 		int green = (pixel >>> 8) & mask;
 		int blue = pixel & mask;
-		
+
 		return Color.rgb(red, green, blue);
 	}
 
-	public Collection<Number> getModel(){
-		//This converts the 2D Node<Integer> array to a 1D array and then sends all the ints to a list
+	public Collection<Number> getModel() {
+		// This converts the 2D Node<Integer> array to a 1D array and then sends all the
+		// ints to a list
 		return Arrays.stream(model).flatMap(i -> Arrays.stream(i)).map(e -> e.getValue()).collect(Collectors.toList());
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,9 @@
-module atu.software {
+module VisitorLab {
 	requires java.desktop;
-	
+
 	requires javafx.base;
 	requires javafx.graphics;
 	requires transitive javafx.controls;
-	
+
 	exports ie.atu.sw;
 }
